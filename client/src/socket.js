07910432a635fe3e60537dc9f94fb5814afc7f51
DHIAKANZARI <dhiakanzari444@gmail.com>
2026-04-28 13:59:44 +0200

    fix socket url

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
-// Use same host but port 3000 if dev, or relative if deployed
-const URL = import.meta.env.DEV ? 'https://storyaa.onrender.com' : '/';
+const URL = 'https://storyaa.onrender.com';
+
 export const socket = io(URL, {
   autoConnect: true
 });
